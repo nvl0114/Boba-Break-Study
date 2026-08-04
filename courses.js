366e61449e807567d3b0c9e669cb442de3a2771e
nvl0114 <nnnnnadia14@gmail.com>
2026-08-04 10:26:50 +0800

    Update courses.js

--- courses.js
+++ courses.js
@@ -1,18 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+    // Search Course
     const searchInput = document.getElementById("searchInput");
     const cards = document.querySelectorAll(".card");
 
-    searchInput.addEventListener("input", () => {
-        const keyword = searchInput.value.toLowerCase();
+    if (searchInput) {
+        searchInput.addEventListener("input", () => {
+            const keyword = searchInput.value.toLowerCase();
 
-        cards.forEach(card => {
-            const title = card.querySelector("h3").textContent.toLowerCase();
+            cards.forEach(card => {
+                const title = card.querySelector("h3").textContent.toLowerCase();
 
-            if (title.includes(keyword)) {
-                card.style.display = "flex";
-            } else {
-                card.style.display = "none";
-            }
+                if (title.includes(keyword)) {
+                    card.style.display = "flex";
+                } else {
+                    card.style.display = "none";
+                }
+            });
         });
-    });
+    }
+
+
+    // Dropdown Courses Sidebar
+    const coursesToggle = document.getElementById("coursesToggle");
+    const courseSubmenu = document.getElementById("courseSubmenu");
+
+    if (coursesToggle && courseSubmenu) {
+        coursesToggle.addEventListener("click", (e) => {
+            e.preventDefault();
+
+            courseSubmenu.classList.toggle("show");
+        });
+    }
+
 });
